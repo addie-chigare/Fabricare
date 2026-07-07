31861717371084015ff66ea6a65a2d6150f33a43
addie-chigare <adichigare@gmail.com>
2026-07-07 16:27:34 +0530

    chore: remove hardcoded MONGO_URI fallback and use environment variable

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const mongoURI = process.env.MONGO_URI || "mongodb+srv://Aditya:Aditya12345@cluster0.xtlsnno.mongodb.net/ecomshop";
+    const mongoURI = process.env.MONGO_URI;
     const conn = await mongoose.connect(mongoURI);
 
     console.log(`MongoDB Connected sucessfully.....`);
