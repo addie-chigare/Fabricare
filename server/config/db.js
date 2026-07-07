f00f1ccf5448647ca23efaa489025a68bafa2655
addie-chigare <adichigare@gmail.com>
2026-07-07 12:25:16 +0530

    chore: make database URI dynamic in config/db.js

--- server/config/db.js
+++ server/config/db.js
@@ -2,9 +2,8 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(
-      "mongodb+srv://Aditya:Aditya12345@cluster0.xtlsnno.mongodb.net/ecomshop",
-    );
+    const mongoURI = process.env.MONGO_URI || "mongodb+srv://Aditya:Aditya12345@cluster0.xtlsnno.mongodb.net/ecomshop";
+    const conn = await mongoose.connect(mongoURI);
 
     console.log(`MongoDB Connected sucessfully.....`);
   } catch (error) {
